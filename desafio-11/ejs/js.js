2d174cd8cbb378c5f6a2e3deb1e6d35f1a7c029a
Alejo Cecco <acecco6@gmail.com>
2021-09-17 19:14:56 -0300

    desafio-11

---
+++ desafio-11/ejs/js.js
@@ -0,0 +1,93 @@
+const express= require("express") 
+
+// express datos
+const app=express()
+const port=8080
+
+
+
+// Declaracion de ruta api
+const api=express.Router()
+app.use("/api",api)
+
+// Declaracion de ruta Static
+app.use(express.static("./public"))
+// lectura JSON de api
+api.use(express.json());
+api.use(express.urlencoded({extended: true}));
+
+
+// productos array
+var productos=[]
+var id_producto=0
+
+
+const server =app.listen(port,()=>{
+    console.log(`Servidor ${server.address().port}`)
+})
+server.on("error",error=>console.log("Error en el Servidor",error))
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+// Declaracion Rutas
+
+app.get("/productos/vista",(req,res)=>{
+    if (productos.length==0) {
+        res.render('pages/index', { item: "No hay productos" ,active:false});
+    }else{
+        res.render('pages/index', { item: productos,active:true});
+    }
+    
+})
+
+api.get("/productos",(req,res)=>{
+    if (productos.length > 0) {
+        res.json(productos)
+    }else{
+        res.json({error:"No hay Productos cargados"})
+    }
+})
+
+api.get("/productos/:id",(req,res)=>{
+    let item=[]
+    for (const producto of productos) {
+        if (producto.id == req.params.id) {item.push(producto)}
+    }
+    if (item.length>0) {
+        res.json(item)
+    }else{
+        res.json({error:"Producto no Encontrado"})
+    }
+    
+})
+
+api.post("/productos/guardar",(req,res)=>{
+    let body=req.body
+    body.id=id_producto+1
+    id_producto++
+    productos.push(body)
+    res.redirect("/")
+})
+
+api.put("/productos/actualizar/:id",(req,res)=>{
+    let id=req.params.id
+    let item=req.body
+    for (const producto of productos) {
+        if(id==producto.id){
+            producto.Title=item.Title
+            producto.Price=item.Price
+            producto.Thumbnail=item.Thumbnail
+        }
+    }
+    res.json(item)
+})
+
+api.delete("/productos/borrar/:id",(req,res)=>{
+    let id=req.params.id
+    let item=productos.find(items=>items.id==id)
+    let items=productos.filter(e=>e.id != id)
+    productos=items
+    res.json(item)
+})
